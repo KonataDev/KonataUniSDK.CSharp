f47d1fcfc05dcaa48fd82c912128c6d1f786ff04
littlenine12 <56431872+littlenine12@users.noreply.github.com>
2021-07-26 02:04:44 +0800

    Upload Code

---
+++ MyExtensionExample/MyExtensionExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using KonataCSharp.SDK.EventArgs.Enums;
+using KonataCSharp.SDK.EventArgs.Events;
+using KonataCSharp.SDK.EventArgs.Interfaces;
+
+namespace MyExtensionExample
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            KonataCSharp.SDK.Core.Program.Main(args);
+        }
+    }
+
+    internal class MyExtensionExample : IOnStartup, IGroupMessage
+    {
+        public KonataEventReturnType OnGroupMessage(GroupMessageEventArgs e)
+        {
+            Console.WriteLine("Recv GroupMessage:\t" + e.Message);
+
+            if (e.Message == "/ping")
+            {
+                var newmsgid = e.Api.SendGroupMessage(e.Bot, e.FromGroup, "Hi! I'm Konata. -Test").Result;
+
+                Console.WriteLine("Send msgid:\t" + newmsgid);
+            }
+
+            return KonataEventReturnType.Ignore;
+        }
+
+        public bool OnStartup(StartupEventArgs e)
+        {
+            return true;
+        }
+    }
+}
